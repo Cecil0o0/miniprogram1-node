85c6b2bb377fae30c17e1812407833372c6cbaac
清风 <jiegithub@gmail.com>
2018-11-25 17:07:04 +0800

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,9 +3,6 @@ module.exports = {
     {
       name: 'modelService',
       script: 'src/index.js',
-      env: {
-        NODE_ENV: 'development'
-      },
       env_production: {
         NODE_ENV: 'production'
       }
